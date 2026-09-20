969d69e7a63f248bd1416a641faf74e05ceea673
dave-woods85 <dave.woods85@outlook.com>
2026-09-20 08:22:42 -0400

    Unit 6 Discussion Complete w/real world scenario (ReadMe updated)

--- unit6_hash_tables/README.md
+++ unit6_hash_tables/README.md
@@ -28,5 +28,21 @@ After completing the programming assignment, add this reflection to your initial
 Your reflection should be approximately 150–200 words and address the following questions:
 
 1. What concepts or skills did you learn while completing this assignment?
+
+This assignment was relatively easy since we didn't have to create any functions. I basically learned how to use Python's
+built-in dictionary functionality, which is useful!
+
 2. What challenges did you encounter, and how did you overcome them?
+
+No real challenges here other than looking up what functions/methods were available for Python's dictionary and then
+making sure to the get syntax right. I tried to challenge myself with the real-world scenario by using a for loop to
+update the dictionary values for a master inventory based on the days transactions.
+
 3. Explain how hash tables behave, what collisions are, and how hash tables can improve efficiency.
+
+Has tables behave like the Python dictionary where a key-value pair can be inserted. It appears, just based off my
+limited testing that Python's dictionary uses a linear method for insertion as adding to the dictionary simply appends
+to the end. Collisions happen when two items attempt to occupy the same bucket. Has tables can deal with this in a few
+different ways to include:
+- chaining: adding multiple items to a bucket
+- open addressing: various probing methods to search for empty buckets
--- unit6_hash_tables/unit6_discussion.py
+++ unit6_hash_tables/unit6_discussion.py
@@ -32,8 +32,19 @@ def main():
     # 4. Display the contents of the dictionary.
 
 
+
     print("\n=== INSERT OPERATIONS ===")
     print("TODO: Create a dictionary and add multiple key-value pairs.")
+
+    # new empty dictionary
+    hash_table =  {}
+    # Adding 5 students with student ID keys
+    hash_table.update({1 : "Aaron", 2 : "Blake", 3 : "Christopher", 4 : "David", 5 : "Eric"})
+
+    # Similarly to a hash table, a dictionary stores key-value pairs in "buckets".
+    # It also has the ability to update values for an existing key.
+
+    print(hash_table)
 
     # ===============================
     # TODO (Student): LOOKUP OPERATIONS
@@ -46,6 +57,12 @@ def main():
 
     print("\n=== LOOKUP OPERATIONS ===")
     print("TODO: Demonstrate successful key lookups.")
+    print("Student Roster:")
+    for s in hash_table: # retrieve each individual student from the table
+        print(f"ID: {s} Name: " + hash_table.get(s))
+
+    # The lookup uses the provided key and searches the entire table for that key. If it exists the associated value
+    # is returned.
 
     # ===============================
     # TODO (Student): UPDATE OPERATIONS
@@ -59,6 +76,16 @@ def main():
 
     print("\n=== UPDATE OPERATIONS ===")
     print("TODO: Demonstrate updating an existing key.")
+    # Printing the current dictionary
+    print("Current Roster:")
+    for s in hash_table: # retrieve each individual student from the table
+        print(f"ID: {s} Name: " + hash_table.get(s))
+    # Updating 2 entries
+    hash_table.update({1 : "Alex", 3 : "Charles"})
+    # Printing the updated dictionary
+    print("Updated Roster:")
+    for s in hash_table: # retrieve each individual student from the table
+        print(f"ID: {s} Name: " + hash_table.get(s))
 
     # ===============================
     # TODO (Student): DELETE OPERATIONS
@@ -71,6 +98,17 @@ def main():
 
     print("\n=== DELETE OPERATIONS ===")
     print("TODO: Demonstrate deleting a key-value pair.")
+    # Printing the current dictionary
+    print("Current Roster:")
+    for s in hash_table: # retrieve each individual student from the table
+        print(f"ID: {s} Name: " + hash_table.get(s))
+    # Removing 2 Students
+    del hash_table[1]
+    del hash_table[3]
+    # printing updated dictionary
+    print("Updated Roster:")
+    for s in hash_table: # retrieve each individual student from the table
+        print(f"ID: {s} Name: " + hash_table.get(s))
 
     # ===============================
     # TODO (Student): EDGE CASES
@@ -88,6 +126,51 @@ def main():
 
     print("\n=== EDGE CASES ===")
     print("TODO: Demonstrate and explain edge cases.")
+
+    # Look up missing key
+    print(f"Student ID 6: ",  hash_table.get(6))
+    # Delete a missing key using pop instead of del for safety
+    missing_student = hash_table.pop("Frank", None)
+    print("Missing Student: ", missing_student)
+    # Updating a missing key essentially adds it to the dictionary
+    hash_table.update({6 : "Frank"})
+    print("Updated missing key: " + hash_table.get(6))
+    # Using an empty dictionary returns, wait for it... an empty dictionary
+    empty_dictionary = {}
+    print(empty_dictionary)
+
+    print("\n=== REAL WORLD SCENARIO ===")
+    # Updating the inventory for a used video game store
+    game_inventory = {} # initial inventory is empty
+    print("Purchased initial stock from a game collector.")
+    # purchasing some initial stock of old games
+    day1_bought = {"Rayman" : 3, "Sonic & Knuckles" : 1, "Chrono Trigger" : 4, "Lion King" : 2,
+                         "Final Fantasy VI" : 1, "Paper Mario" : 3}
+    # update the master inventory with day 1 transactions
+    game_inventory.update(day1_bought)
+    # print out day 1 transactions
+    print("Day 1\nWe bought:")
+    for g in game_inventory:
+        print(game_inventory.get(g), "copies of", g)
+    # print out day 2 transactions
+    print("Day2\nWe bought:")
+    day2_bought = {"Acro the Acrobat" : 2, "Oddworld: Abe's Odyssey" : 3}
+    for g in day2_bought:
+        print(day2_bought.get(g), "copies of", g)
+        game_inventory.update({g : day2_bought.get(g)}) # update the master inventory
+
+    day2_sold = {"Rayman" : 1, "Paper Mario" : 2}
+    print("We sold:")
+    for g in day2_sold:
+        print(day2_sold.get(g), "copies of", g)
+        game_inventory.update({g : game_inventory.get(g) - day2_sold.get(g)}) # update the master inventory quantity
+
+    # print the master inventory after transactions
+    print("Current Stock:")
+    for g in game_inventory:
+        print(game_inventory.get(g), "copies of", g)
+
+
 
 
 
